8f4355a5bc53efb58cdc8e7f7d0f178f03dfb6e3
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-09-25 21:33:18 -0300

    Add files via upload

---
+++ Aula POO.py
@@ -0,0 +1,16 @@
+# linguagem = 'python'
+# ide = 'vscode'
+# substituiu = 'linguagemABC'
+
+# print(f {}'é uma linguaguem de programção : {} executado tambem no {}: e foi criada para substituir a {} '.format(linguagem,ide,substituiu)) '
+
+# print ("python e uma linguagem de programacao \n Um dos IDE que executa o arquivo.py e o vs code \n A linguagem de programacao que substituiu foi a linguagemABC s")
+
+print('o que e python ?')
+print('python e uma linguagem de programacao \n')
+print('**********************************************************')
+print('Qual programa (IDE) executa o arquivo.py \n ?')
+print('Um dos programas que executa o arquivo.py e o vscode \n')
+print('***********************************************************')
+print('Qual a linguagem de programacao python substituiu \n')
+print('Ela substituiu a linguagem ABC de origem Holandesa \n')
